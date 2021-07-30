a7d268b6d47ff0b83441ae783fd235d9149d0231
xodud001 <51165061+xodud001@users.noreply.github.com>
2021-07-30 16:34:18 +0900

    refact : app.py 수정

--- app.py
+++ app.py
@@ -1,13 +1,13 @@
 from flask import Flask
-from flask-restx import Api, Resource
+from flask_restx import Api, Resource
 
 app = Flask(__name__)
 api = Api(app)
 
-@app.route('/')
-def hello_world():
-    return 'Hello World!'
+@api.route('/hello')
+class HelloWorld(Resource):
+    def get(self):
+        return {"hello": "world!"}
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     app.run(debug=True, host='0.0.0.0', port=9001)
-
